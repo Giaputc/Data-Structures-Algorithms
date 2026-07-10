e074b58ea071a540fba825697a0bcad34c70b410
giaputc <phamg501@gmail.com>
2026-07-10 10:43:49 +0700

    bai 2

---
+++ leetcode/leetcode75/1679.py
@@ -0,0 +1,16 @@
+nums=[3,1,3,4,3]
+k=6
+nums.sort()
+l,r=0,len(nums)-1
+ope=0
+while l<r:
+    sum=nums[l]+nums[r]
+    if sum==k:
+        ope+=1
+        l+=1
+        r-=1
+    elif sum<k:
+        l+=1
+    elif sum>k:
+        r-=1
+print(ope)
